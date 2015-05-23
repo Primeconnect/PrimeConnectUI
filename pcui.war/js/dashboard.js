76b205ae5d14648081962f3a1a52e80701b0bbf7
William <william237@gmail.com>
2015-05-23 18:55:45 -0400

    Merge branch 'master' of https://github.com/Primeconnect/PrimeConnectUI.git
    Conflicts:
    	pcui.war/content-pages/dashboard.html
    	pcui.war/index.html
    	pcui.war/js/dashboard.js
    	pcui.war/js/login.js

--- pcui.war/js/dashboard.js
+++ pcui.war/js/dashboard.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 
 var app = angular.module('batsDashboard', []);
@@ -42,46 +41,3 @@ var app = angular.module('batsDashboard', []);
 	$scope.getSchedules();
 	
 }]);
-=======
-'use strict';
-
-var app = angular.module('batsDashboard', []);
-
- app.controller('dashboardCtrl', function($scope,$http) {
-	$scope.logout = function() {
-		$scope.$emit('logout');
-	};
-	
-	//$scope.professionals = getDoctors();
-	
-	$scope.getDoctors = function() {
-		$http.get('/pcservice/rest/login/google?email=chetancmehta@gmail.com').
-		  success(function(data, status, headers, config) {
-		    // this callback will be called asynchronously
-		    // when the response is available
-		    $scope.professionals = data.result;
-		  }).
-		  error(function(data, status, headers, config) {
-		    // called asynchronously if an error occurs
-		    // or server returns response with an error status.
-		  });
-	};
-	
-	$scope.getSchedules = function() {
-		$http.get('/pcservice/rest/schedule?email=chetancmehta@gmail.com').
-		  success(function(data, status, headers, config) {
-		    // this callback will be called asynchronously
-		    // when the response is available
-		    $scope.schedules = data.result;
-		  }).
-		  error(function(data, status, headers, config) {
-		    // called asynchronously if an error occurs
-		    // or server returns response with an error status.
-		  });
-	};
-	
-	$scope.getDoctors();
-	$scope.getSchedules();
-	
-});
->>>>>>> branch 'master' of https://github.com/Primeconnect/PrimeConnectUI.git
